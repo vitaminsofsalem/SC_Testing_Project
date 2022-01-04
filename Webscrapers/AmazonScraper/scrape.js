ae3cde0e8b06dd76e35d53040f1f3c813ac5b853
vitaminsofsalem <jokydokyduck@gmail.com>
2022-01-04 20:25:25 +0200

    Added accidentally deleted AmazonScraper File

---
+++ Webscrapers/AmazonScraper/scrape.js
@@ -0,0 +1,74 @@
+import cheerio from "cheerio";
+import express from "express";
+import axios from "axios";
+import iphoneSchema from "../../Database/models/iphone.schema";
+const PORT = 3000;
+const app = express();
+
+connectToMongod = async () => {
+  await mongoose.connect(
+    "mongodb+srv://sohila2001:Sohila2001@cluster0.wvl0d.mongodb.net/Iphone?retryWrites=true&w=majority",
+    { useNewUrlParser: true }
+  );
+  console.log("connected to MongoDb");
+};
+
+const iLoveFakhry = async () => {
+  const data = [];
+
+  for (var i = 1; i <= 4; i++) {
+    const res = await axios(
+      `https://www.amazon.com/s?k=iphone&page=${i}&qid=1641303333&sprefix=iphone%2Caps%2C262&ref=sr_pg_${i}`
+    );
+
+    const html = res.data;
+    const $ = cheerio.load(html);
+
+    $(".s-asin").each((i, el) => {
+      if (i == 16) {
+        return false;
+      } else {
+        const title = $(el)
+          .find(".s-title-instructions-style")
+          .end()
+          .find(".a-size-medium.a-color-base.a-text-normal")
+          .html();
+
+        const price = $(el)
+          .find(".s-price-instructions-style")
+          .end()
+          .find(".a-offscreen")
+          .html();
+
+        const link =
+          "https://amazon.com" +
+          $(el)
+            .find(".s-title-instructions-style")
+            .end()
+            .find(".a-link-normal")
+            .attr("href");
+
+        data.push({
+          title,
+          price,
+          link,
+        });
+        iphoneSchema.create({
+          title,
+          price,
+          link,
+        });
+      }
+    });
+  }
+
+  return data;
+};
+
+app.get("/scrape", async (req, res) => {
+  await connectToMongod();
+  const data = await iLoveFakhry();
+  res.send(data);
+});
+
+app.listen(PORT, () => console.log(`Server Started on PORT ${PORT}`));
