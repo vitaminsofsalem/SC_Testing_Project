d1c490f5fe3c7bbb5459689a6f3ff9ba3871eb73
vitaminsofsalem <jokydokyduck@gmail.com>
2022-01-10 03:30:11 +0200

    Added minor comments to getRequestsCache.js for clarity

--- CachedRequests/getRequestCached.js
+++ CachedRequests/getRequestCached.js
@@ -18,7 +18,7 @@ const cachedFilePath = (dir, file) => `${dir}/${file}.cache`;
  * @param {string} URL whose response will be cached
  * @param {int} lifetimeMS lifetime of the cache in milliseconds (before it becomes stale)
  * @param {string} cachePath path where the cached data are stored
- * @returns {any} reponse that has been recently cached
+ * @returns {any} response that has been recently cached
  */
 const cacheResponse = async (url, lifetimeMS, cachePath) => {
   try {
@@ -29,7 +29,7 @@ const cacheResponse = async (url, lifetimeMS, cachePath) => {
       data: Buffer.from(JSON.stringify(res)).toString("base64"), //encode data as base64 then store it
       timestampMS: Date.now().toString(16), //store timestamp in unix format (seconds since 1970) (the time when the page was fetched)
       lifetimeMS: lifetimeMS.toString(16), //store lifetime value
-      //both timestamp and lifetime are in milliseconds and encoded in base 16
+      // * * both timestamp and lifetime are in MILLISECONDS and encoded in base 16
     };
 
     await fs.promises.writeFile(cachePath, JSON.stringify(toBeStored));
@@ -67,13 +67,13 @@ const getRequestCached = async (url) => {
     await fs.promises.access(filePath, fs.constants.F_OK); //check if file exists
     const cacheObj = JSON.parse(await fs.promises.readFile(filePath));
     if (checkIfStale(cacheObj)) {
-      const res = cacheResponse(url, CACHE_LIFETIME_SECONDS * 1000, filePath);
+      const res = cacheResponse(url, CACHE_LIFETIME_SECONDS * 1000, filePath); //**multiply by 1000 to convert to milliseconds
       return res;
     }
     const res = JSON.parse(Buffer.from(cacheObj.data, "base64").toString());
     return res;
   } catch (e) {
-    const res = cacheResponse(url, CACHE_LIFETIME_SECONDS * 1000, filePath);
+    const res = cacheResponse(url, CACHE_LIFETIME_SECONDS * 1000, filePath); //**multiply by 1000 to convert to milliseconds
     return res;
   }
 };
